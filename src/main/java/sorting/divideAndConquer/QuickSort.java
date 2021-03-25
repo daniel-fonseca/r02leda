1dded174a2c0554f3aeb97e1b0d5ecb3222bfedd
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2021-03-25 16:53:14 -0300

    fiz o quick sort, tudo passou nos testes

--- src/main/java/sorting/divideAndConquer/QuickSort.java
+++ src/main/java/sorting/divideAndConquer/QuickSort.java
@@ -14,7 +14,42 @@ public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		boolean isSortable = false;
+		if (leftIndex < rightIndex && rightIndex < array.length) {
+			isSortable = true;
+		}
+		
+		if (isSortable) {
+			int pivotIndex = particiona(array, leftIndex, rightIndex);
+			sort(array, leftIndex, pivotIndex - 1);
+			sort(array, pivotIndex + 1, rightIndex);
+		}
+		
+	}
+	
+	private int particiona(T[] array, int leftIndex, int rightIndex) {
+		int range = rightIndex - leftIndex + 1;
+		int randomPivotIndex = (int) (Math.random() * range) + leftIndex;
+		T aux = array[leftIndex];
+		array[leftIndex] = array[randomPivotIndex];
+		array[randomPivotIndex] = aux;
+		
+		T pivot = array[leftIndex];
+		int i = leftIndex;
+		
+		for (int j = leftIndex + 1; j <= rightIndex; j++) {
+			if (array[j].compareTo(pivot) < 0) {
+				i++;
+				T temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+		
+		T temp = array[i];
+		array[i] = array[leftIndex];
+		array[leftIndex] = temp;
+		
+		return i;
 	}
 }
