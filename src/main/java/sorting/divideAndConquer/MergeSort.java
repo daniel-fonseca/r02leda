4194ea6adc6d55bcd0a5fff5165da9660aa64e0e
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2021-03-26 16:26:53 -0300

    fiz o three way quick sort, tudo esta passando nos testes, nenhuma falha

--- src/main/java/sorting/divideAndConquer/MergeSort.java
+++ src/main/java/sorting/divideAndConquer/MergeSort.java
@@ -4,12 +4,6 @@ import java.util.Arrays;
 
 import sorting.AbstractSorting;
 
-/**
- * Merge sort is based on the divide-and-conquer paradigm. The algorithm
- * consists of recursively dividing the unsorted list in the middle, sorting
- * each sublist, and then merging them into one single sorted list. Notice that
- * if the list has length == 1, it is already sorted.
- */
 public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
--- src/main/java/sorting/divideAndConquer/threeWayQuicksort/ThreeWayQuickSort.java
+++ src/main/java/sorting/divideAndConquer/threeWayQuicksort/ThreeWayQuickSort.java
@@ -5,28 +5,66 @@ import sorting.AbstractSorting;
 public class ThreeWayQuickSort<T extends Comparable<T>> extends
 		AbstractSorting<T> {
 
-	/**
-	 * No algoritmo de quicksort, selecionamos um elemento como pivot,
-	 * particionamos o array colocando os menores a esquerda do pivot 
-	 * e os maiores a direita do pivot, e depois aplicamos a mesma estrategia 
-	 * recursivamente na particao a esquerda do pivot e na particao a direita do pivot. 
-	 * 
-	 * Considerando um array com muitos elementos repetidos, a estrategia do quicksort 
-	 * pode ser otimizada para lidar de forma mais eficiente com isso. Essa melhoria 
-	 * eh conhecida como quicksort tree way e consiste da seguinte ideia:
-	 * - selecione o pivot e particione o array de forma que
-	 *   * arr[l..i] contem elementos menores que o pivot
-	 *   * arr[i+1..j-1] contem elementos iguais ao pivot.
-	 *   * arr[j..r] contem elementos maiores do que o pivot. 
-	 *   
-	 * Obviamente, ao final do particionamento, existe necessidade apenas de ordenar
-	 * as particoes contendo elementos menores e maiores do que o pivot. Isso eh feito
-	 * recursivamente. 
-	 **/
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
+		boolean isSortable = false;
+		if (leftIndex < rightIndex && rightIndex < array.length) {
+			isSortable = true;
+		}
+		
+		if (isSortable) {
+			int pivotIndex = particionaMenores(array, leftIndex, rightIndex);
+			sort(array, leftIndex, pivotIndex - 1);
+			int i = particionaIguais(array, leftIndex, rightIndex, pivotIndex);
+			sort(array, i + 1, rightIndex);
+			
+		}
+		
+		
 	}
-
+	
+	private int particionaMenores(T[] array, int leftIndex, int rightIndex) {
+		
+		int range = rightIndex - leftIndex - 1;
+		int randomPivotIndex = (int) (Math.random() * range) + leftIndex;
+		T aux = array[leftIndex];
+		array[leftIndex] = array[randomPivotIndex];
+		array[randomPivotIndex] = aux;
+		
+		T pivot = array[leftIndex];
+		int i = leftIndex;
+		
+		for (int j = i + 1; j <= rightIndex; j++) {
+			if (array[j].compareTo(pivot) < 0) {
+				i++;
+				T temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+		
+		T temp = array[i];
+		array[i] = array[leftIndex];
+		array[leftIndex] = temp;
+		int pivotIndex = i;
+		
+		return i;
+	}
+	
+	private int particionaIguais(T[] array, int leftIndex, int rightIndex, int pivotIndex) {
+		int i = pivotIndex;
+		T pivot = array[pivotIndex];
+		
+		for (int j = i+1; j <= rightIndex; j++) {
+			if (array[j].compareTo(pivot) == 0) {
+				i++;
+				T temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+		
+		return pivotIndex;
+	}
 }
